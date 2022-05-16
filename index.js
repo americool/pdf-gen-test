ee32667ffa705e049b8d29aa7948544f6c8cd44c
Abe Anderson <abe@abes-mbp.myfiosgateway.com>
2022-05-16 16:04:45 -0400

    a bit better nees some work

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch(); // launch a browser (chromium by default but you can chose another one)
+  const browser = await puppeteer.launch({
+      defaultViewport: {
+          width: 850,
+          height: 1100
+      }
+  }); // launch a browser (chromium by default but you can chose another one)
   const page = await browser.newPage(); // open a page in the browser
   await page.goto("http://localhost:3000/", {
     waitUntil: "networkidle2",
@@ -11,9 +16,10 @@ const puppeteer = require("puppeteer");
   await page.click('.cy--login-submit');
   await page.waitForNavigation();
 
-  await page.goto("http://localhost:3000/reviews/2/dumb-test", {
+  await page.goto("http://localhost:3000/2/dumb-test", {
     waitUntil: "networkidle0",
   }); // visit the printable version of your page
-  await page.pdf({ format: "a4", path: "./my_file.pdf" }); // generate the PDF 🎉
+
+  await page.pdf({ path: "./my_file.pdf" }); // generate the PDF 🎉
   await browser.close(); // don't forget to close the browser. Otherwise, it may cause performances issues or the server may even crash..
 })();
